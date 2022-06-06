97d6aad73e6a9abbeaf8a32c4387337cb15e7cb6
Jonathan Gonzalez <78762194+jonnnhhh@users.noreply.github.com>
2022-06-06 16:26:51 -0700

    Add files via upload

--- hdl/motor_logic.v
+++ hdl/motor_logic.v
@@ -18,52 +18,64 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-module motor_logic #(parameter count_limit = 0,parameter duty_cycle = 0)
+module motor_logic #(parameter count_limit = 0)
 (
-input I_servo_pwm_EN,
-input I_clk,
-output reg o_servo_output = 0,
-output movement_done
+    input       I_servo_pwm_EN,
+    input [2:0] I_duty_cycle_selected,
+    input       I_clk,
+    output reg  o_servo_output = 0,
+    output      movement_done
 );
 
-//clk divider
-//reg [14:0] count_limit = 6249;//10khz
-reg [14:0] counter = 0;
-reg o_motor_clk = 0;
-//pwm
-reg [8:0] counter_pwm = 0;
-reg movement_done_reg = 0;
-wire counter_trigger;
-//localparam duty_cycle = 500; //n-1 
-assign counter_trigger = (I_servo_pwm_EN)?1:0; 
-assign movement_done = movement_done_reg;
-      
+//----------------------------------------------------
+
+reg [8:0]  counter_pwm              = 0;
+reg [13:0] counter_continous_signal = 0;
+reg        movement_done_reg        = 0;
+wire       counter_trigger;
+
+//----------------------------------------------------
+
+assign counter_trigger = (I_servo_pwm_EN)? 1 : 0;
+assign movement_done   = movement_done_reg;
+
+//----------------------------------------------------
 always@(posedge I_clk)
 begin
 
-   if(counter_pwm >= duty_cycle)
-   
-   begin 
-   
-   counter_pwm <= 0; 
-   movement_done_reg <= 1;
-   o_servo_output <= 0;
-   
-   end
-   
-   else if (counter_trigger == 1 || counter_pwm > 0 )
-   
-   begin 
-   
-   counter_pwm <= counter_pwm+1;
-   movement_done_reg <= 0;
-   o_servo_output <= 1;
-   
-   end
-    
-end 
+    if(counter_pwm >= I_duty_cycle_selected)
+    begin
 
-   
+      counter_pwm          <= 0;
+      o_servo_output       <= 0;
+
+           if(counter_continous_signal >= count_limit)
+           begin
+
+                movement_done_reg         <= 1;
+                counter_continous_signal  <= 0;
+
+           end
+
+      end
+
+    else if (counter_trigger == 1 || counter_pwm > 0 )
+    begin
+
+      movement_done_reg    <= 0;
+      counter_pwm          <= counter_pwm+1;
+      o_servo_output       <= 1;
+
+      end
+
+    if(counter_continous_signal < count_limit)
+    begin
+
+    counter_continous_signal <= counter_continous_signal+1;
+
+    end
+end
+//----------------------------------------------------
 endmodule
 
 
